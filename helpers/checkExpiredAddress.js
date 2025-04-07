340b16c106f29322694814395aba876aadbadbfc
Tushar1357 <tushartuteja19@gmail.com>
2025-04-07 15:02:12 +0530

    fix the expiry logic

--- helpers/checkExpiredAddress.js
+++ helpers/checkExpiredAddress.js
@@ -39,6 +39,7 @@ const checkExpiredAddress = async (tgId) => {
         await Wallets.update(
           {
             status: "unpaid",
+            createdAt: new Date(new Date(wallet.createdAt).getTime() + 60 * 1000)
           },
           {
             where: {
