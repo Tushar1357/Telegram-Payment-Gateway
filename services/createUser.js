086f20a11c705fe738a6a0129687a3a66b946b14
Tushar1357 <tushartuteja19@gmail.com>
2025-04-08 12:33:18 +0530

    fixed new user logic

--- services/createUser.js
+++ services/createUser.js
@@ -2,12 +2,19 @@ const User = require("../database/models/users/User.js");
 
 const createUser = async (tgId, tgName, tgUserName) => {
   try {
-    await User.create({
-      tgId,
-      tgName,
-      tgUserName,
-      subscriptionStatus: false,
+    const user = await User.findOne({
+      where: {
+        tgId,
+      },
     });
+    if (!user) {
+      await User.create({
+        tgId,
+        tgName,
+        tgUserName,
+        subscriptionStatus: false,
+      });
+    }
   } catch (error) {
     console.log("Error while creating user. Error:", error?.message);
   }
