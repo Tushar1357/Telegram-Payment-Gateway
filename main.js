61e21b1e52962c3f07d779a52d3cfba053eaaf9b
Tushar1357 <tushartuteja19@gmail.com>
2025-04-08 16:21:55 +0530

    added more detail

--- main.js
+++ main.js
@@ -170,9 +170,9 @@ bot.on("callback_query", async (query) => {
     } else if (status === "changed_chain") {
       await bot.sendMessage(
         chatId,
-        `You have changed the chain from ${
+        `Note: _You have changed the chain from ${
           choice === "base" ? "BSC" : "BASE"
-        } to ${choice.toUpperCase()}\n\n💰 USDC Amount: *${MIN_AMOUNT}*\n\n📥 Send only *USDC (${chainLabel})* to:\n\`${address}\`\n\n⏳ You have 30 minutes to complete the payment. Your address will expire at ${expiry.toUTCString()}\n❗ If you pay late, please contact support at @Skelter10 or @MrBean000.\n\n✅ *Important Notes:*\n- No need to send transaction hash or screenshot.\n- Your deposit will be detected automatically. (Will roughly take 15-20 seconds.)\n- Transaction fees must be covered by you.\n- Make sure the amount is *not less* than the required *${MIN_AMOUNT} USDC*.\n- Send only *(${chainLabel}) USDC* (${
+        } to ${choice.toUpperCase()}._\n\n💰 USDC Amount: *${MIN_AMOUNT}*\n\n📥 Send only *USDC (${chainLabel})* to:\n\`${address}\`\n\n⏳ You have 30 minutes to complete the payment. Your address will expire at ${expiry.toUTCString()}\n❗ If you pay late, please contact support at @Skelter10 or @MrBean000.\n\n✅ *Important Notes:*\n- No need to send transaction hash or screenshot.\n- Your deposit will be detected automatically. (Will roughly take 15-20 seconds.)\n- Transaction fees must be covered by you.\n- Make sure the amount is *not less* than the required *${MIN_AMOUNT} USDC*.\n- Send only *(${chainLabel}) USDC* (${
           choice === "bsc" ? "Binance smart chain" : "Base Chain"
         }). Sending from other networks may result in loss of funds.`,
         {
@@ -182,7 +182,7 @@ bot.on("callback_query", async (query) => {
     } else if (status === "old") {
       await bot.sendMessage(
         chatId,
-        `You are already having a pending payment of 10$ on ${choice.toUpperCase()} chain. Please complete that or wait for 30 minutes to generate a new address.\n\n💰 USDC Amount: *${MIN_AMOUNT}*\n\n📥 Send only *USDC (${chainLabel})* to:\n\`${address}\`\n\n⏳ You have 30 minutes to complete the payment. Your address will expire at ${expiry.toUTCString()}\n❗ If you pay late, please contact support at @Skelter10 or @MrBean000.\n\n✅ *Important Notes:*\n- No need to send transaction hash or screenshot.\n- Your deposit will be detected automatically. (Will roughly take 15-20 seconds.)\n- Transaction fees must be covered by you.\n- Make sure the amount is *not less* than the required *${MIN_AMOUNT} USDC*.\n- Send only *(${chainLabel}) USDC* (${
+        `Note: _You are already having a pending payment of 10$ on ${choice.toUpperCase()} chain. Please complete that or wait for 30 minutes to generate a new address._\n\n💰 USDC Amount: *${MIN_AMOUNT}*\n\n📥 Send only *USDC (${chainLabel})* to:\n\`${address}\`\n\n⏳ You have 30 minutes to complete the payment. Your address will expire at ${expiry.toUTCString()}\n❗ If you pay late, please contact support at @Skelter10 or @MrBean000.\n\n✅ *Important Notes:*\n- No need to send transaction hash or screenshot.\n- Your deposit will be detected automatically. (Will roughly take 15-20 seconds.)\n- Transaction fees must be covered by you.\n- Make sure the amount is *not less* than the required *${MIN_AMOUNT} USDC*.\n- Send only *(${chainLabel}) USDC* (${
           choice === "bsc" ? "Binance smart chain" : "Base Chain"
         }). Sending from other networks may result in loss of funds.`,
         {
